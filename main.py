cc278b9dba5229b45766335ba79d0369e35266d1
MoongrallABC <168622772+MoongrallABC@users.noreply.github.com>
2024-05-15 19:19:01 +0000

    Commit changes

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def is_palindrome(word):
-    for i in range(len(word) // 2):
-        if word[i] != word[-i - 1]:
-            return False
-    return True
+def smallest_number(a, b, c):
+    return min(a, b, c)
 
-word = input("Gryut: ").lower()
-if is_palindrome(word):
-    print("This word is a palindrome.")
-else:
-    print("This word is not a palindrome.")
+num1 = float(input("876:"))
+num2 = float(input("978: "))
+num3 = float(input("1186: "))
+
+smallest = smallest_number(num1, num2, num3)
+print("The smallest number is: ", num2)
